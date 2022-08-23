2288323a23ec5b633b1ad1d8c0f3b8972e18d67e
PHILIP <philophilo037@gmail.com>
2022-08-23 10:54:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
+// // Write your code hereconst num1 = 31
+//     const num2 = 2
+//     const multiply = num1 * num2;
+
+//     const random = Math.floor(Math.random() * 10) +1;
+
+//     const num3 = 31
+//     const num4 = 9
+//     const mod = num3 % num4;
+
+//     const max = Math.max(5,10,15,20)
+const num1 = 62;
+const num2 = 1;
+const num3 = 19;
+const num4 = 5;
+
 // Write your code here
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random() * 10);
+const mod = num3 % num4;
+const max = Math.max(3, 1, 20, 8);
